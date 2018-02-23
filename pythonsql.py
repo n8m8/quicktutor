7c705a69408d3b1dd6e5a475f924e3a38fd635b7
axm862 <axm862@case.edu>
2018-02-23 04:45:38 -0500

    Added the sqlite3 database schema along with the python code for creating and connecting to the database.

---
+++ pythonsql.py
@@ -0,0 +1,96 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    """ creating a database connection to a SQLite database """
+    try:
+        conn = sqlite3.connect(db_file)
+        # print(sqlite3.version)
+        return conn
+    except Error as e:
+        print(e)
+    # finally:
+    #     conn.close()
+    return None
+
+def create_table(conn, create_table_sql):
+    # create a table from the create_table_sql statement
+    # :param conn: Connection object
+    # :param create_table_sql: a CREATE TABLE statement
+    # :return:
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def main():
+    database = "pythonsqlite.db"
+    sql_create_users_table = """CREATE TABLE IF NOT EXISTS users (
+                                userid integer NOT NULL,
+                                email text NOT NULL,
+                                password text NOT NULL,
+                                displayname text NOT NULL,
+                                PRIMARY KEY(userid)
+                                );"""
+    sql_create_classes_table = """CREATE TABLE IF NOT EXISTS classes (
+                                    classid integer NOT NULL,
+                                    dept text NOT NULL,
+                                    c_number integer NOT NULL,
+                                    PRIMARY KEY(classid)
+                                    );"""
+    sql_create_ruserclasses_table = """CREATE TABLE IF NOT EXISTS ruserclasses (
+                                        r_userid integer NOT NULL,
+                                        r_classid integer NOT NULL,
+                                        FOREIGN KEY (r_userid) REFERENCES users (userid),
+                                        FOREIGN KEY (r_classid) REFERENCES classes (classid)
+                                        );"""
+    sql_create_listings_table = """CREATE TABLE IF NOT EXISTS listings (
+                                    listingid integer NOT NULL,
+                                    l_userid integer NOT NULL,
+                                    l_classid integer NOT NULL,
+                                    shortDescription text NOT NULL,
+                                    time_Stamp text NOT NULL,
+                                    PRIMARY KEY(listingid),
+                                    FOREIGN KEY (l_userid) REFERENCES users(userid),
+                                    FOREIGN KEY (l_classid) REFERENCES classes(classid)
+                                    );"""
+    sql_create_helppairs_table = """CREATE TABLE IF NOT EXISTS helpPairs (
+                                    pairid integer NOT NULL,
+                                    h_listingid integer NOT NULL,
+                                    tutorid integer NOT NULL,
+                                    fieldname text NOT NULL,
+                                    time_Stamp text NOT NULL,
+                                    PRIMARY KEY(pairid),
+                                    FOREIGN KEY (h_listingid) REFERENCES listings (listingid)
+                                    );"""
+    sql_create_messages_table = """CREATE TABLE IF NOT EXISTS messages (
+                                    messagenumber integer NOT NULL,
+                                    m_pairid integer NOT NULL,
+                                    messagecontents text NOT NULL,
+                                    time_Stamp text NOT NULL,
+                                    PRIMARY KEY(messagenumber),
+                                    FOREIGN KEY (m_pairid) REFERENCES helpPairs(pairid)
+                                    );"""
+
+    conn = create_connection(database)
+    if conn is not None:
+        # create users table
+        create_table(conn, sql_create_users_table)
+        # create classes table
+        create_table(conn, sql_create_classes_table)
+        # create ruserclasses table
+        create_table(conn, sql_create_ruserclasses_table)
+        # create listings table
+        create_table(conn, sql_create_listings_table)
+        # create helppairs table
+        create_table(conn, sql_create_helppairs_table)
+        # create messages table
+        create_table(conn, sql_create_messages_table)
+        conn.commit()
+    else:
+        print("Error! cannot create the database connection.")
+
+
+if __name__ == '__main__':
+    main()
