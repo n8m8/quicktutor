d317d139b3cca47e3e804d30f3b0eb0f2398ff3b
axm862 <axm862@case.edu>
2018-05-04 15:17:42 -0400

    live server testing

--- test.py
+++ test.py
@@ -46,21 +46,21 @@ class MyTest(TestCase):
 
 if __name__== '__main__':
 	unittest.main()
-#
-# from flask import Flask
-# from flask_testing import LiveServerTestCase
-#
-# class MyTest(LiveServerTestCase):
-#
-#     def create_app(self):
-#         app = Flask(__name__)
-#         app.config['TESTING'] = True
-#         # Default port is 5000
-#         app.config['LIVESERVER_PORT'] = 5000
-#         # Default timeout is 5 seconds
-#         app.config['LIVESERVER_TIMEOUT'] = 10
-#         return app
-#
-#     def test_server_is_up_and_running(self):
-#         response = urllib2.urlopen(self.get_server_url())
-#         self.assertEqual(response.code, 200)
+
+from flask import Flask
+from flask_testing import LiveServerTestCase
+
+class MyTest(LiveServerTestCase):
+
+    def create_app(self):
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        # Default port is 5000
+        app.config['LIVESERVER_PORT'] = 5000
+        # Default timeout is 5 seconds
+        app.config['LIVESERVER_TIMEOUT'] = 10
+        return app
+
+    def test_server_is_up_and_running(self):
+        response = urllib2.urlopen(self.get_server_url())
+        self.assertEqual(response.code, 200)
